f07dfad207afbef06e738ce378a24355e34b15f2
MindOverGame <42360536+MindOverGame@users.noreply.github.com>
2018-08-24 17:48:14 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 /**
  * Authors: Manuel Paredes-Castaneda and Noah Anderson
- * Date: 2018/21/08
+ * Date: 2018/23/08
  *
  * A simple hello world program in C
  *
@@ -11,6 +11,7 @@
 int main(int argc, char **argv) {
 
   printf("Manuel Paredes-Castaneda: Pre-Engineering\n");
+  printf("I'm Noah Anderson and I'm majoring in Computer Science.\n");
 
   return 0;
 }
